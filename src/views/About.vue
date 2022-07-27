6397de2573acd092df386be781bd54c49a47c6fb
diegobarbosa <diego.gutemberg@hotmail.com>
2022-07-27 10:11:40 -0300

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <main class="about-page">
+    <h1>About</h1>
+    <p>this is de about page</p>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage',
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="home-page">
+    <h1>Home</h1>
+    <p>this is de home page</p>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+}
+</script>
+
+<style lang="scss">
+
+</style>
